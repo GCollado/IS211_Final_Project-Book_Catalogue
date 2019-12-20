e997c9e9a31b4b287815d0a5187f2aac279d5a57
GCollado <gabriel.collado@spsmail.cuny.edu>
2019-12-20 08:30:40 -0500

    Add files via upload

--- Final_Project-Db.py
+++ Final_Project-Db.py
@@ -1,30 +1,32 @@
-import _sqlite3
+import sqlite3
 
-
-connection = _sqlite3.connect("memmory:")
+connection = sqlite3.connect(":memory:")
 
 cursor = connection.cursor()
 
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS books(book_id INT PRIMARY KEY,
- book_title TEXT, page_count TEXT, average_rating TEXT""")
+ book_title TEXT, page_count TEXT, average_rating TEXT)""")
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS authors(author_id INT PRIMARY KEY,
- author_name TEXT""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS authors(author_id INTEGER PRIMARY KEY,
+ author_name TEXT)""")
 
 user_input = []
-user_input = input("Please enter book tittle, number of pages, and the average rating: " )
+book = input("Please enter book tittle: ")
+pages =   input("Please enter the number of pages: ")
+avg_rating = input("Please enter the average rating: ")
+responses = [book, pages, avg_rating]
+user_input.append(responses)
+print(user_input)
 
 cursor.execute("""INSERT OR IGNORE INTO books(book_title, page_count, average_rating)
-VALUES (?,?,?)""",user_input)
+VALUES (?,?,?)""",(responses[0][0], responses[0][1], responses[0][2]))
 
-cursor.execute("""INSERT OR IGNORE INTO books(book_title, page_count, average_rating)
-VALUES (?,?,?)""",user_input)
+cursor.execute("INSERT OR IGNORE INTO authors(author_name) VALUES (?)""", user_input)
 
-cursor.execute("INSERT OR IGNOR INTO authors(author_name) VALUES (?)""", user_input)
-
-cursor.commit()
+connection.commit()
 
 connection.close()
+
 def main():
     pass
