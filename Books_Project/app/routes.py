3c2081674971c3dc9845c87b0cc6bf62643b2338
GCollado <gabriel.collado@spsmail.cuny.edu>
2020-12-24 23:27:34 -0500

    Updated 'register' view function in routes.py
    Corrected  RegistrationForm to RegistrationForm() in line 75.

--- Books_Project/app/routes.py
+++ Books_Project/app/routes.py
@@ -72,7 +72,7 @@ def user_logout():
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = RegistrationForm
+    form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data)
         user.set_password(form.password.data)
